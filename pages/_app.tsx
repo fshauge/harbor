19c9a1f0e8a18a1ce8ccee81518a9932ed9215bd
Markus Hauge <markus.hauge@hotmail.com>
2021-11-28 15:27:38 +0100

    Add Auth0 sign in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import { UserProvider } from "@auth0/nextjs-auth0";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
-const Home = () => {
-  return <h1>Hello world!</h1>;
-};
+import { useUser } from "@auth0/nextjs-auth0";
 
-export default Home;
+export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
+}
