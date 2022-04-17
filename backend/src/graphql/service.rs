78729d24c4c1703652d5a4c280796b714f6eca1b
Markus Hauge <markus.hauge@hotmail.com>
2022-04-17 23:44:34 +0200

    Add mutations for starting/stopping services

--- backend/src/graphql/service.rs
+++ backend/src/graphql/service.rs
@@ -1,8 +1,7 @@
 use super::application::Application;
 use crate::model;
 use async_graphql::{Context, Object, Result};
-use bollard::{image::BuildImageOptions, Docker};
-use futures::StreamExt;
+use bollard::Docker;
 use sqlx::PgPool;
 
 #[derive(Default)]
@@ -58,34 +57,25 @@ impl ServiceMutation {
     async fn build_service(&self, ctx: &Context<'_>, id: i32) -> Result<String> {
         let pool = ctx.data::<PgPool>()?;
         let service = model::Service::by_id(id, pool).await?;
-        let application = service.application(pool).await?;
         let docker = ctx.data::<Docker>()?;
-
-        let remote = format!(
-            "{}#{}:{}",
-            application.repository, application.branch, service.build_context
-        );
-
-        let t = format!("{}-{}", application.name, service.image);
-
-        let options = BuildImageOptions {
-            remote,
-            t,
-            rm: true,
-            ..Default::default()
-        };
-
-        let build_info = docker
-            .build_image(options, None, None)
-            .collect::<Vec<_>>()
-            .await
-            .into_iter()
-            .collect::<Result<Vec<_>, _>>()?
-            .into_iter()
-            .filter_map(|b| b.stream)
-            .collect::<String>();
-
+        let build_info = service.build(pool, docker).await?;
         Ok(build_info)
+    }
+
+    async fn start_service(&self, ctx: &Context<'_>, id: i32) -> Result<String> {
+        let pool = ctx.data::<PgPool>()?;
+        let service = model::Service::by_id(id, pool).await?;
+        let docker = ctx.data::<Docker>()?;
+        let container_id = service.start(pool, docker).await?;
+        Ok(container_id)
+    }
+
+    async fn stop_service(&self, ctx: &Context<'_>, id: i32) -> Result<String> {
+        let pool = ctx.data::<PgPool>()?;
+        let service = model::Service::by_id(id, pool).await?;
+        let docker = ctx.data::<Docker>()?;
+        let container_id = service.stop(pool, docker).await?;
+        Ok(container_id)
     }
 }
 
--- backend/src/model/service.rs
+++ backend/src/model/service.rs
@@ -1,5 +1,12 @@
 use super::Application;
+use anyhow::{bail, Result};
+use bollard::{
+    container::{Config, CreateContainerOptions},
+    image::BuildImageOptions,
+    Docker,
+};
 use chrono::NaiveDateTime;
+use futures::StreamExt;
 use sqlx::{Error, FromRow, PgPool};
 
 #[derive(FromRow)]
@@ -62,10 +69,95 @@ impl Service {
         .fetch_one(pool)
         .await
     }
+
+    pub async fn update_container_id(
+        id: i32,
+        container_id: Option<&str>,
+        pool: &PgPool,
+    ) -> Result<Service, Error> {
+        sqlx::query_as!(
+            Service,
+            r"
+            UPDATE services
+            SET container_id = $2
+            WHERE id = $1
+            RETURNING *
+            ",
+            id,
+            container_id
+        )
+        .fetch_one(pool)
+        .await
+    }
 }
 
 impl Service {
     pub async fn application(&self, pool: &PgPool) -> Result<Application, Error> {
         Application::by_id(self.application_id, pool).await
+    }
+
+    pub async fn build(&self, pool: &PgPool, docker: &Docker) -> Result<String> {
+        let application = self.application(pool).await?;
+
+        let remote = format!(
+            "{}#{}:{}",
+            application.repository, application.branch, self.build_context
+        );
+
+        let t = format!("{}-{}", application.name, self.image);
+
+        let options = BuildImageOptions {
+            remote,
+            t,
+            rm: true,
+            ..Default::default()
+        };
+
+        let build_info = docker
+            .build_image(options, None, None)
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .filter_map(|b| b.stream)
+            .collect::<String>();
+
+        Ok(build_info)
+    }
+
+    pub async fn start(&self, pool: &PgPool, docker: &Docker) -> Result<String> {
+        if self.container_id.is_some() {
+            bail!("service is running");
+        }
+
+        let application = self.application(pool).await?;
+        let t = format!("{}-{}", application.name, self.image);
+        let options = CreateContainerOptions { name: t.clone() };
+
+        let config = Config {
+            image: Some(t),
+            ..Default::default()
+        };
+
+        let container_id = docker.create_container(Some(options), config).await?.id;
+
+        docker
+            .start_container::<String>(&container_id, None)
+            .await?;
+
+        Service::update_container_id(self.id, Some(&container_id), pool).await?;
+        Ok(container_id)
+    }
+
+    pub async fn stop(&self, pool: &PgPool, docker: &Docker) -> Result<String> {
+        let container_id = match self.container_id.clone() {
+            Some(container_id) => container_id,
+            None => bail!("service is not running"),
+        };
+
+        docker.remove_container(&container_id, None).await?;
+        Service::update_container_id(self.id, None, pool).await?;
+        Ok(container_id)
     }
 }
